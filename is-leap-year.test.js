2625ed142127622a5c6bd5b8103331c539d6b20c
Luis Bardalez <lbardalez@gmail.com>
2022-10-09 18:56:41 -0500

    [green] 800 is a leap year

--- is-leap-year.test.js
+++ is-leap-year.test.js
@@ -9,8 +9,16 @@ describe('A year is not a leap year if not divisible by 4', () => {
 })
 
 describe('A year is a leap year if divisible by 4', () => {
-    test('8 us a leap year', () => {
+    test('8 is a leap year', () => {
         const year = 8
+        const result = isLeapYear(year)
+        expect(result).toBe(true)
+    })
+})
+
+describe('A year is a leap year if divisible by 400', () => {
+    test('800 is a leap year', () => {
+        const year = 800
         const result = isLeapYear(year)
         expect(result).toBe(true)
     })
